c45ea38090ab24b42a6a4bdea76441fc49554d89
jungminji <wjdalswl0215@gmail.com>
2024-12-28 20:46:17 +0900

    Square 컴포넌트 생성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React, { StrictMode } from "react";
+
+/** 웹 브라우저와 상호작용하는 React의 라이브러리 */
 import { createRoot } from "react-dom/client";
+
 import "./styles.css";
 
 import App from "./App";
