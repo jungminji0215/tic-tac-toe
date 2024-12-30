c8bc2ceec7e13ddf62c112064af60975998084e8
jungminji <wjdalswl0215@gmail.com>
2024-12-31 01:41:10 +0900

    기본 게임 동작 완성

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    // 사각형이 이미 채워져 있으면 조기 return
-    if (squares[i]) {
+    // 사각형이 이미 채워져 있으면 조기 return || 승부가 결정 났으면 return
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -24,6 +24,14 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   /**
    * 참고
    * 리액트에서는 주로 이벤트를 나타내는 prop 에는 onSomething 과 같은 이름을 사용하고
@@ -31,6 +39,7 @@ export default function Board() {
    */
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -48,6 +57,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 function Square({ value, onSquareClick }) {
